bc4e0a8295b7d0e7096c6d5f7880bf2041685d16
Mohammad Dohadwala <mohammad@dohad.dev>
2021-06-01 16:56:45 +0400

    case insensitive checking

--- source/enableButton.js
+++ source/enableButton.js
@@ -1,10 +1,10 @@
 const enableButton = (button) => {
   if (button.classList.contains('awsui-button-no-text')) return
 
-  const textKeys = ['Delete', 'Empty', 'Terminate'];
+  const textKeys = ['delete', 'empty', 'terminate'];
 
   textKeys.forEach(key => {
-    if (!button.innerText.startsWith(key)) return;
+    if (!button.innerText.toLowerCase().startsWith(key)) return;
 
     button.classList.remove('awsui-button-disabled');
     button.disabled = false
